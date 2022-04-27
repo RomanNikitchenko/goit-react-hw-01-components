61cc78ff632b941d177482020eaeb240dc7ac848
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2022-04-27 21:20:27 +0300

    Update profile.jsx

--- src/components/social-profile/profile.jsx
+++ src/components/social-profile/profile.jsx
@@ -6,9 +6,9 @@ const Profile = ({ username, tag, location, avatar, stats }) => {
         <div class="profile">
             <div class="description">
                 <img
-                    src={avatar}
-                    // alt="User avatar"
-                    // class="avatar"
+                    src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+                    alt="User avatar"
+                    class="avatar"
                 />
                 <p class="name">Petra Marica</p>
                 <p class="tag">@pmarica</p>
