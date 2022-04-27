97562871233384f2c994416392bfd12ce9b1d9ae
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2022-04-27 20:37:37 +0300

    com3

--- src/components/social-profile/profile.jsx
+++ src/components/social-profile/profile.jsx
@@ -1,4 +1,4 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 const Profile = ({ username, tag, location, avatar, stats }) => {
     const { followers, views, likes } = stats;
@@ -33,16 +33,16 @@ const Profile = ({ username, tag, location, avatar, stats }) => {
     )
 };
 
-Profile.propTypes = {
-    username: PropTypes.string.isRequired,
-    tag: PropTypes.string.isRequired,
-    location: PropTypes.string.isRequired,
-    avatar: PropTypes.string.isRequired,
-    stats: PropTypes.shape({
-        followers: PropTypes.number.isRequired,
-        views: PropTypes.number.isRequired,
-        likes: PropTypes.number.isRequired,
-    }),
-};
+// Profile.propTypes = {
+//     username: PropTypes.string.isRequired,
+//     tag: PropTypes.string.isRequired,
+//     location: PropTypes.string.isRequired,
+//     avatar: PropTypes.string.isRequired,
+//     stats: PropTypes.shape({
+//         followers: PropTypes.number.isRequired,
+//         views: PropTypes.number.isRequired,
+//         likes: PropTypes.number.isRequired,
+//     }),
+// };
 
 export default Profile;
